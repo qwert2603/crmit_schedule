7106c47323497cdaa8a1836c945d1a9f9290994e
alex <qwert2603@mail.ru>
2019-03-22 18:47:08 +0700

    _schedulePlatform

--- android/app/src/main/java/com/qwert2603/crmit_schedule/MainActivity.java
+++ android/app/src/main/java/com/qwert2603/crmit_schedule/MainActivity.java
@@ -1,13 +1,37 @@
 package com.qwert2603.crmit_schedule;
 
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        MethodChannel methodChannel = new MethodChannel(getFlutterView(), "app.channel.schedule");
+
+        methodChannel
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            if (call.method.contentEquals("getAuthedTeacherIdOrZero")) {
+                                result.success(1);
+                                return;
+                            }
+                            if (call.method.contentEquals("getAccessToken")) {
+                                result.success("ac28bd98-afc4-4310-90ee-017515a636ba");
+                                return;
+                            }
+                            if (call.method.contentEquals("on401")) {
+                                result.success("ok");
+                                methodChannel.invokeMethod("clearCache", null);
+                                return;
+                            }
+                            result.notImplemented();
+                        }
+                );
+    }
 }
--- lib/repo.dart
+++ lib/repo.dart
@@ -4,10 +4,26 @@ import 'package:crmit_schedule/cache.dart';
 import 'package:crmit_schedule/const.dart';
 import 'package:crmit_schedule/entity.dart';
 import 'package:crmit_schedule/state.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+
+const _schedulePlatform = const MethodChannel('app.channel.schedule');
 
 class Repo {
   final Cache _cache = Cache();
+
+  Repo() {
+    _schedulePlatform.setMethodCallHandler((call) {
+      if (call.method == 'clearCache') {
+        print("Repo clearCache");
+        return Future.wait([
+          _cache.clearTeachers(),
+          _cache.clearSchedule(),
+        ]);
+      }
+      return Future.error("not implemented");
+    });
+  }
 
   Future<ScheduleInitialModel> getScheduleInitialModel(
       int teacherId, bool allowCache) {
@@ -51,7 +67,7 @@ class Repo {
   }
 
   Future<List<DayOfWeek>> getScheduleGroups(int teacherId) async {
-    final url = _addAccessToken(
+    final String url = await _addAccessToken(
         "$BASE_URL$API_PREFIX/schedule${teacherId != null ? "?teacherId=$teacherId" : ""}");
     final http.Response response = await http.get(url);
     _checkStatusCode(response);
@@ -61,27 +77,31 @@ class Repo {
   }
 
   Future<List<Teacher>> getTeachersList() async {
-    final url = _addAccessToken("$BASE_URL$API_PREFIX/teachers_list");
+    final String url =
+        await _addAccessToken("$BASE_URL$API_PREFIX/teachers_list");
     final http.Response response = await http.get(url);
     _checkStatusCode(response);
     final List list = json.decode(response.body);
     return list.map((q) => Teacher.fromJson(q)).toList();
   }
 
-  Future<int> getAuthedTeacherIdOrZero() async {
-    return 1;
-  }
+  Future<int> getAuthedTeacherIdOrZero() async =>
+      _schedulePlatform.invokeMethod("getAuthedTeacherIdOrZero");
 
   void _checkStatusCode(http.Response response) {
     if (response.statusCode < 200 || response.statusCode >= 300) {
+      if (response.statusCode == 401) {
+        _schedulePlatform.invokeMethod("on401");
+      }
       throw Exception("response.statusCode == ${response.statusCode}");
     }
   }
 
-  String _addAccessToken(String url) =>
-      "$url${url.contains('?') ? "&" : "?"}access_token=${_getAccessToken()}";
+  Future<String> _addAccessToken(String url) async =>
+      "$url${url.contains('?') ? "&" : "?"}access_token=${await _getAccessToken()}";
 
-  String _getAccessToken() => "ac28bd98-afc4-4310-90ee-017515a636ba";
+  Future<String> _getAccessToken() =>
+      _schedulePlatform.invokeMethod("getAccessToken");
 
   List<DayOfWeek> _makeSchedule(List<ScheduleGroup> scheduleGroups) {
     final List<DayOfWeek> result = [];
